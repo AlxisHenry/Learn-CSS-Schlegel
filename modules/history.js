5d9c86b3107a6e5a285bea4f497ac97242b0d3aa
AlxisHenry <alexis.henry150357@gmail.com>
2022-02-08 11:40:02 +0100

    🏌️‍♂️# History work / Adding color red/green on span / Adding animation when error work

--- modules/history.js
+++ modules/history.js
@@ -1,5 +1,5 @@
 import { this_find_sport, Research_History, Span_History } from "./data.js";
-import { this_contain_value as GET_ID } from "./data.js";
+import { Adapt_Value } from "./data.js";
 
 export function Adding_to_Research_History() {
   if (Research_History.includes(this_find_sport.value)) {
@@ -13,7 +13,6 @@ export function Resize_Research_History() {
   if (Research_History.length > 3) {
     Research_History.pop();
   }
-  console.log(Research_History)
 }
 
 export function Show_Research_History() {  
@@ -21,23 +20,26 @@ export function Show_Research_History() {
     Span_History[0].innerHTML = "";
   } else {
     Span_History[0].innerHTML = `${Research_History[0]}`;
+    Checking_History_Value(Span_History[0])
   }
   if ((Span_History[1].innerHTML = `${Research_History[1]}` === "undefined")) {
     Span_History[1].innerHTML = "";
   } else {
     Span_History[1].innerHTML = `${Research_History[1]}`;
+    Checking_History_Value(Span_History[1])
   }
   if ((Span_History[2].innerHTML = `${Research_History[2]}` === "undefined")) {
     Span_History[2].innerHTML = "";
   } else {
     Span_History[2].innerHTML = `${Research_History[2]}`;
+    Checking_History_Value(Span_History[2]);
   }
 }
 
 export function Checking_History_Value(id) {
-  if (document.querySelector(`#${id}`)) {
-    id.classList.add('valid');
+  if (document.querySelector(`#${Adapt_Value(id)}`)) {
+    id.style.color = "green";
   } else {
-    id.classList.add('not-valid');
+    id.style.color = "red";
   }
 }
--- modules/routes.js
+++ modules/routes.js
@@ -1,22 +1,27 @@
 import { this_find_sport } from "./data.js";
 import { this_contain_value as GET_ID } from "./data.js";
+import { INPUT_RESEARCH } from "./data.js";
 
 export function Verification_Research_Content() {
   if (this_find_sport.value === "") {
     console.log("null");
+    INPUT_RESEARCH.classList.add('border-error-animation')
     // Ajout d'une animation si la case est vide. {error anim}
   } else {
+    INPUT_RESEARCH.classList.remove('border-error-animation');
     Verification_Research_Validity();
   }
 }
 
 export function Verification_Research_Validity() {
   if (document.getElementById(`${GET_ID}`)) {
+    INPUT_RESEARCH.classList.remove('border-error-animation');
     this_find_sport.href = `#${GET_ID}`;
-
     // Ajout d'une animation d'envoi vers l'image.
     // Ajout d'une classe à celle-ci.
   } else {
+    INPUT_RESEARCH.classList.add('border-error-animation')
+    alert("Nous n'avons pas ce sport, désolé !");
     // Ajout d'une animation si le sport n'existe pas. {error anim}
   }
 }
