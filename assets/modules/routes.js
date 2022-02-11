954ea837533216c11860ff1fcac4f10eaa8bb0ed
AlxisHenry <alexis.henry150357@gmail.com>
2022-02-11 09:40:36 +0100

    🏌️‍♂️# User can connect with his username

---
+++ assets/modules/routes.js
@@ -0,0 +1,26 @@
+import { this_find_sport } from "./data.js";
+import { INPUT_RESEARCH } from "./data.js";
+
+export function Verification_Research_Content() {
+  if (this_find_sport.value === "") {
+    console.log("null");
+    INPUT_RESEARCH.classList.add('border-error-animation')
+    // Ajout d'une animation si la case est vide. {error anim}
+  } else {
+    INPUT_RESEARCH.classList.remove('border-error-animation');
+    Verification_Research_Validity();
+  }
+}
+
+export function Verification_Research_Validity() {
+  if (document.getElementById(`${this_find_sport.value}`)) {
+    INPUT_RESEARCH.classList.remove('border-error-animation');
+    this_find_sport.href = `#${this_find_sport.value}`;
+    // Ajout d'une animation d'envoi vers l'image.
+    // Ajout d'une classe à celle-ci.
+  } else {
+    INPUT_RESEARCH.classList.add('border-error-animation')
+    alert("Nous n'avons pas ce sport, désolé !");
+    // Ajout d'une animation si le sport n'existe pas. {error anim}
+  }
+}
