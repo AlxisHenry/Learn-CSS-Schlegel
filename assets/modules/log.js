6895364d7999a5f6a717b56c01b12dacf1875bf3
AlxisHenry <alexis.henry150357@gmail.com>
2022-02-18 11:36:00 +0100

    🏌️‍♂️# Fix 404 error

--- assets/modules/log.js
+++ assets/modules/log.js
@@ -72,7 +72,7 @@ export function LOGOUT_USER() {
 }
 
 function REDIRECT_TO_HOMEPAGE() {
-  window.location.replace("./../index.html");
+  window.location.replace("./index.html");
 } 
 
 export function TOGGLE_VIEW_PW() {
