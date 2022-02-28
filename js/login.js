0ad90fa943def5cdb6f967c17c0b2a2ed53eb587
AlxisHenry <alexis.henry150357@gmail.com>
2022-02-28 14:21:04 +0100

    🏌️‍♂️# Clean main branch

--- js/login.js
+++
@@ -1,8 +0,0 @@
-// LOCAL CONNECTION
-import { VERIFIY_REGISTER_VALUES, ADD_TO_LOCALSTORAGE } from "../assets/modules/log.js";
-import { GET_SUBMIT } from "../assets/modules/data/global.js";
-
-GET_SUBMIT.addEventListener("click", () => {
-  VERIFIY_REGISTER_VALUES();
-  ADD_TO_LOCALSTORAGE();
-});
